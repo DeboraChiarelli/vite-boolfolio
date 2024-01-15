0c58f49e291cc7100b5446b246dce91cabc18768
DeboraChiarelli <deborachiarelli92@gmail.com>
2024-01-15 16:22:07 +0100

    Add - ProjectCard component

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    props: {
+        project: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+  
+
+<template>
+    <div class="card project-card">
+        <h2>{{ project.title }}</h2>
+
+    </div>
+</template>
+  
+
+<style lang="scss" scoped></style>
