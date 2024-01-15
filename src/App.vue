170e4be8c2c6efad104ce2ccef0673d4215f0793
DeboraChiarelli <deborachiarelli92@gmail.com>
2024-01-15 16:11:45 +0100

    Add - chiamata api in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,44 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      projects: [],
+      BASE_URL: 'http://localhost:8000/api'
+    };
+  },
+  methods: {
+    fetchProjects() {
+      axios.get(`${this.BASE_URL}/projects`)
+        .then((res) => {
+          console.log(res);
+          this.projects = res.data;
+        });
+    }
+  },
+  created() {
+    this.fetchProjects();
+  }
+}
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <div class="container">
+      <h1>
+        Lista Progetti
+      </h1>
+    </div>
+
+    <div class="container">
+      <div>
+        <div v-for="project in projects" :key="project.id" class="card project-card">
+          <h2>{{ project.title }}</h2>
+        </div>
+      </div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style lang="scss" scoped></style>
