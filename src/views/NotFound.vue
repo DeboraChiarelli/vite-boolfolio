62a039532b284be625474c25d5420470d557fa74
DeboraChiarelli <deborachiarelli92@gmail.com>
2024-01-17 11:51:53 +0100

    Add - notfound component

---
+++ src/views/NotFound.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>404 - Pagina non trovata</h1>
+        <p>La pagina che stai cercando non esiste. Torna alla <router-link :to="{ name: 'home' }">pagina
+                iniziale</router-link>.</p>
+    </div>
+</template>
+  
+<script>
+export default {
+    created() {
+        console.log('Il componente NotFound è stato creato.');
+    },
+};
+</script>
+  
+<style scoped>
+h1 {
+    color: #ff0000;
+}
+</style>
